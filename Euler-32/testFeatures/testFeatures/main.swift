34a15d615dbdb12edcc3de4c9bf0dceff847570e
Alan Hawse <arh@Alans-MacBook-Pro-2.local>
2015-10-11 19:36:06 -0400

    Initial Commit

---
+++ Euler-32/testFeatures/testFeatures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testFeatures
+//
+//  Created by Alan Hawse on 10/11/15.
+//  Copyright (c) 2015 Alan Hawse. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
